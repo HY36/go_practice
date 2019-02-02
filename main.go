816d06682d1b68271d9f40da8dee07393c547268
huoyan <895350445@qq.com>
2019-02-02 21:36:02 +0800

    leetcode: 977. Squares of a Sorted Array

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func transpose(A [][]int) [][]int {
 	line, column := len(A), len(A[0])
@@ -15,8 +18,36 @@ func transpose(A [][]int) [][]int {
 	return B
 }
 
+func shortestToChar(S string, C byte) []int {
+	var result []int
+	front, last := 0, 0
+	for index, value := range S {
+		if byte(value) == C {
+			last = index
+			for i := 0; i <= (last-front)/2; i++ {
+				result = append(result, i)
+			}
+			front = index
+		}
+	}
+	return result
+}
+
+func sortedSquares(A []int) []int {
+	for indx, value := range A {
+		A[indx] = value * value
+	}
+	sort.Ints(A)
+	return A
+}
+
 func main() {
 	A := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
 	B := transpose(A)
 	fmt.Println(B)
+	c := shortestToChar("loveleetcode", 'e')
+	fmt.Println(c)
+	C := []int{-4, -1, 0, 3, 10}
+	result := sortedSquares(C)
+	fmt.Println(result)
 }
